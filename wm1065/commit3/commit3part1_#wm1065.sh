18062805b2564168253dc1cce710e3c3003f00bc
wm1065 <wm1065@nyu.edu>
2019-10-13 16:41:04 -0400

    Modified commit3part1_#wm1065.sh to support moving html files with certain patterns

--- wm1065/commit3/commit3part1_#wm1065.sh
+++ wm1065/commit3/commit3part1_#wm1065.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-# create html_folder if it does not exist
-echo "Creating html_folder..."
-if [ -d "html_folder" ]; then
-	echo "html_folder already exists"
+#
+# this is a script that moves html files which contain a given pattern string 
+# in their file names to html_folder
+#
+# the pattern string is given in the first argument
+#
+# if the pattern string is not given, the script will move all htmls
+#
+
+TARGET_FOLDER="html_folder"
+
+echo "Creating $TARGET_FOLDER..."
+if [ -d $TARGET_FOLDER ]; then
+	echo "$TARGET_FOLDER already exists"
 else
-	mkdir html_folder 
-	echo "html_folder created"
+	mkdir $TARGET_FOLDER 
+	echo "$TARGET_FOLDER created"
 fi
 
-FILES=*.html
+# set file names pattern
+FILES="*${1:-*}*.html"
 
 # allow match-no-files pattern to expand to a null string
-# so that we will not treat *.html as an actual html file
+# so we will not treat *.html as an actual html file anymore
 shopt -s nullglob
 
-echo -e "\nMoving html files to html_folder..."
+echo -e "\nMoving qualified html files to $TARGET_FOLDER..."
 
-# move each file to html_folder
+# move files to $FOLDER
 for file in $FILES
 do
-	mv $file html_folder
-	echo "File $file moved to html_folder"
+	mv $file $TARGET_FOLDER
+	echo "File $file moved to $TARGET_FOLDER"
 done
 
 echo "All files moved"

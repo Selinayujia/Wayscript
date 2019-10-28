4b18bfe1766c55ea5f7f791e54c1b50989815438
Mindy Ruan <mr4739@nyu.edu>
2019-10-28 19:40:52 -0400

    initial commit of django setup script, checks for and installs python/pip

---
+++ mr4739/django__setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Work in progress django setup script. Do not use this version.
+
+# First install Python and pip if they are not already installed.
+echo "Installing python3 if not installed."
+sudo apt install python3
+
+echo "Installing pip if not installed."
+sudo apt-get install python3-pip
+
+echo "Script complete."

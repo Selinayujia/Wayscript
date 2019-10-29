fe9945c4e69ebb74393f217af8126bf7baca1a21
Mindy Ruan <mr4739@nyu.edu>
2019-10-28 20:37:37 -0400

    Added standard django setup, changed python3/pip3 to python/pip

--- mr4739/django__setup.sh
+++ mr4739/django__setup.sh
@@ -2,13 +2,23 @@
 # Work in progress django setup script. Do not use this version.
 
 # First install Python and pip if they are not already installed.
-echo "Installing python3 if not installed."
-sudo apt install python3
+echo "Installing python if not installed..."
+sudo apt install python
 
-echo "Installing pip if not installed."
-sudo apt-get install python3-pip
+echo "Installing pip if not installed..."
+sudo apt-get install python-pip
 
-echo "Installing requirements."
-sudo pip3 install -r requirements.txt
+# Install all requirements listed in requirements.txt.
+echo "Installing requirements..."
+sudo pip install -r requirements.txt
+
+# Set up django project.
+echo "Setting up django project..."
+django-admin startproject mysite_tmp
+cp mysite_tmp/manage.py .
+mv mysite_tmp/mysite_tmp/ mysite
+rm -rf mysite_tmp
+git add manage.py
+git add mysite/*.py
 
 echo "Script complete."
